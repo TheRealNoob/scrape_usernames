5fa47ac9b41fef11e5ac1e53fe0b300ec78c80e9
Michael Timmerman <mike1118@live.com>
2021-05-23 13:28:57 -0500

    initial commit

---
+++ inputs.py
@@ -0,0 +1,92 @@
+game_modes = (
+    'hiscore_oldschool',  # Hiscores
+    "hiscore_oldschool_ironman",  # Ironman
+    'hiscore_oldschool_ultimate',  # Ultimate Ironman
+    'hiscore_oldschool_hardcore_ironman',  # Hardcore Ironman
+    'hiscore_oldschool_deadman',  # Deadman Mode
+    'hiscore_oldschool_seasonal',  # Leagues
+    'hiscore_oldschool_tournament'  # Tournament
+)
+
+categories = (
+    "overall?category_type=0?table=0",  # overall
+    "overall?category_type=0?table=1",  # Attack
+    "overall?category_type=0?table=2",  # Defence
+    "overall?category_type=0?table=3",  # Strength
+    "overall?category_type=0?table=4",  # Hitpoints
+    "overall?category_type=0?table=5",  # Ranged
+    "overall?category_type=0?table=6",  # Prayer
+    "overall?category_type=0?table=7",  # Magic
+    "overall?category_type=0?table=8",  # Cooking
+    "overall?category_type=0?table=9",  # Woodcutting
+    "overall?category_type=0?table=10",  # Fletching
+    "overall?category_type=0?table=11",  # Fishing
+    "overall?category_type=0?table=12",  # Firemaking
+    "overall?category_type=0?table=13",  # Crafting
+    "overall?category_type=0?table=14",  # Smithing
+    "overall?category_type=0?table=15",  # Mining
+    "overall?category_type=0?table=16",  # Herblore
+    "overall?category_type=0?table=17",  # Agility
+    "overall?category_type=0?table=18",  # Thieving
+    "overall?category_type=0?table=19",  # Slayer
+    "overall?category_type=0?table=20",  # Farming
+    "overall?category_type=0?table=21",  # Runecraft
+    "overall?category_type=0?table=22",  # Hunter
+    "overall?category_type=0?table=23",  # Construction
+    "overall?category_type=1?table=1"  # Bounty Hunter - Hunter
+    "overall?category_type=1?table=2"  # Bounty Hunter - Rogue
+    "overall?category_type=1?table=3"  # Clue Scrolls (all)
+    "overall?category_type=1?table=4"  # Clue Scrolls (beginner)
+    "overall?category_type=1?table=5"  # Clue Scrolls (easy)
+    "overall?category_type=1?table=6"  # Clue Scrolls (medium)
+    "overall?category_type=1?table=7"  # Clue Scrolls (hard)
+    "overall?category_type=1?table=8"  # Clue Scrolls (elite)
+    "overall?category_type=1?table=9"  # Clue Scrolls (master)
+    "overall?category_type=1?table=10"  # LMS - Rank
+    "overall?category_type=1?table=11"  # Soul Wars Zeal
+    "overall?category_type=1?table=12"  # Abyssal Sire
+    "overall?category_type=1?table=13"  # Alchemical Hydra
+    "overall?category_type=1?table=14"  # Barrows Chests
+    "overall?category_type=1?table=15"  # Bryophyta
+    "overall?category_type=1?table=16"  # Callisto
+    "overall?category_type=1?table=17"  # Cerberus
+    "overall?category_type=1?table=18"  # Chambers of Xeric
+    "overall?category_type=1?table=19"  # Chambers of Xeric: Challenge Mode
+    "overall?category_type=1?table=20"  # Chaos Elemental
+    "overall?category_type=1?table=21"  # Chaos Fanatic
+    "overall?category_type=1?table=22"  # Commander Zilyana
+    "overall?category_type=1?table=23"  # Corporeal Beast
+    "overall?category_type=1?table=24"  # Crazy Archaeologist
+    "overall?category_type=1?table=25"  # Dagannoth Prime
+    "overall?category_type=1?table=26"  # Dagannoth Rex
+    "overall?category_type=1?table=27"  # Dagannoth Supreme
+    "overall?category_type=1?table=28"  # Deranged Archaeologist
+    "overall?category_type=1?table=29"  # General Graardor
+    "overall?category_type=1?table=30"  # Giant Mole
+    "overall?category_type=1?table=31"  # Grotesque Guardians
+    "overall?category_type=1?table=32"  # Hespori
+    "overall?category_type=1?table=33"  # Kalphite Queen
+    "overall?category_type=1?table=34"  # King Black Dragon
+    "overall?category_type=1?table=35"  # Kraken
+    "overall?category_type=1?table=36"  # Kree'Arra
+    "overall?category_type=1?table=37"  # K'ril Tsutsaroth
+    "overall?category_type=1?table=38"  # Mimic
+    "overall?category_type=1?table=39"  # Nightmare
+    "overall?category_type=1?table=40"  # Obor
+    "overall?category_type=1?table=41"  # Sarachnis
+    "overall?category_type=1?table=42"  # Scorpia
+    "overall?category_type=1?table=43"  # Skotizo
+    "overall?category_type=1?table=44"  # Tempoross
+    "overall?category_type=1?table=45"  # The Gauntlet
+    "overall?category_type=1?table=46"  # The Corrupted Gauntlet
+    "overall?category_type=1?table=47"  # Theatre of Blood
+    "overall?category_type=1?table=48"  # Thermonuclear Smoke Devil
+    "overall?category_type=1?table=49"  # TzKal-Zuk
+    "overall?category_type=1?table=50"  # TzTok-Jad
+    "overall?category_type=1?table=51"  # Venenatis
+    "overall?category_type=1?table=52"  # Vet'ion
+    "overall?category_type=1?table=53"  # Vorkath
+    "overall?category_type=1?table=54"  # Wintertodt
+    "overall?category_type=1?table=55"  # Zalcano
+    "overall?category_type=1?table=56"  # Zulrah
+)
