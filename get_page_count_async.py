a81c1fc1da1dd9ba1c35de7afe4d3b5e42e89de8
Michael Timmerman <mike1118@live.com>
2021-06-06 12:16:24 -0500

    bunch of changes

--- get_page_count_async.py
+++ get_page_count_async.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import os
 import pickle
 import random
 import unicodedata
@@ -15,12 +16,33 @@ from inputs import game_mode_categories, user_agents
 # setup logging
 logger = logging.getLogger()
 handler = logging.FileHandler('page_count.log')
-formatter = logging.Formatter('%(asctime)s   %(levelname)-8s %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(levelname)-8s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.setLevel('DEBUG')
+logger.setLevel(logging.DEBUG)
 
 logger.info('Starting')
+
+async def get_proxy_list(session):
+    """
+    returns the proxy list from webshare.io
+    output format: ['http://user:pass@ip:port', 'http://user:pass@ip:port', ...]
+    """
+    logger.info('fetching proxy list from webshare.io')
+    async with session.get(os.getenv('PROXY_DOWNLOAD_URL')) as response:
+        if response.status == 200:
+            proxies = [proxy.split(':') for proxy in (await response.text()).splitlines()]
+            proxies = [
+                f'http://{proxy[2]}:{proxy[3]}@{proxy[0]}:{proxy[1]}' for proxy in proxies]
+            logging.info(f'{len(proxies)} proxies fetched')
+            return proxies
+        else:
+            logging.error('error fetching proxy list')
+            logging.error(f'response status: {response.status}')
+            logging.error(f'response body: {await response.text()}')
+            # pretty sure that by returning a blank list it'll cause the zip() to throw IndexError
+            # problem for another day if it ever happens
+            return []
 
 def scrape_ranks(html):
     """
@@ -67,110 +89,111 @@ def contains_PageDown_button(html):
 
 
 async def request_webpage(session, proxy, params, game_mode, worker_name):
+    await asyncio.sleep(6)
+    
     header = {"user-agent": random.choice(user_agents)}
     async with session.get(url=f'https://secure.runescape.com/m={game_mode}/overall', headers=header, params=params, proxy=proxy) as response:
         if response.status == 200:
-            if response.history == ():
-                await asyncio.sleep(6)
-                return await response.text()
-            else:
-                logger.error(f'{worker_name}: 200 but redirect??')
-                logger.error(f'{worker_name}: response status: {response.status}')
+            # request was flagged as a robot
+            if '<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">' in await response.text():
+                logger.error(f'{worker_name}: header detected as robot: {header["user-agent"]}')
+                return await request_webpage(session, proxy, params, game_mode, worker_name)
+            # request was redirected
+            if response.history != ():
+                logger.error(f'{worker_name}: request was redirected')
                 logger.error(f'{worker_name}: request url: {response.url}')
                 logger.error(f'{worker_name}: redirect history: {response.history}')
                 logger.error(f'{worker_name}: response body: {await response.text()}')
                 return await request_webpage(session, proxy, params, game_mode, worker_name)
+            # happy path
+            else:
+                return await response.text()
         elif response.status == 502:
-            logger.debug(f'{worker_name}: 502 response from proxy {proxy}.  retrying')
-            await asyncio.sleep(6)
+            # proxy errored
+            logger.error(f'{worker_name}: 502 Bad Gateway response from proxy: {proxy}')
             return await request_webpage(session, proxy, params, game_mode, worker_name)
         elif response.status == 504:
-            logger.debug(f'{worker_name}: 504 response from {response.url}')
-            await asyncio.sleep(6)
+            # hit the HiScores rate limiter
+            logger.debug(f'{worker_name}: 504 Gateway Timeout from HiScores')
             return await request_webpage(session, proxy, params, game_mode, worker_name)
-        else:    
-            logger.warning(f'{worker_name}: abnormal response.  status {response.status_code} redirects {response.history} body {response.text}')
-            await asyncio.sleep(6)
+        else:
+            # unexpected error
+            logger.error(f'{worker_name}: unhandled response.  status: {response.status_code} redirect history: {response.history} body: {response.text}')
             return await request_webpage(session, proxy, params, game_mode, worker_name)
 
 
-async def create_worker(session, proxy, worker_name, game_mode, params):
+async def create_worker(session, proxy, worker_name, game_mode, category):
     """
     creates the task which finds out how many pages are in a hiscores category
     and appends a list of every valid URL for that category
     """
-    logger.debug(f'{worker_name}: worker starting with proxy {proxy} searching {game_mode} / {params}')
+    logger.debug(f'{worker_name}: worker starting with proxy {proxy} searching {game_mode} / {category}')
     global pages_to_scrape
-    # we start with a one-time check to make sure:
-    # 1. if page 1 is blank return 0
-    # 2. if page 1 has names but no page 2 return 1
-    # 3. page 80,000 exists return 80000
-    params['page'] = 1
-    response_text = await request_webpage(session, proxy, params, game_mode, worker_name)
+    _game_mode = game_mode_categories[game_mode][category]['game_mode']
+    _params = game_mode_categories[game_mode][category]['params']
 
-    if is_page_one(response_text):
-        # if page 1 does not have names then kill this worker
-        if len(scrape_ranks(response_text)) == 0:
-            logger.info(f'{worker_name}: page 1 does not have any usernames.  killing worker')
-            logger.debug(f'{worker_name}: worker exiting')
-            return 0 # TODO kill worker
-        # if page 1 has names but is the only page
-        if not contains_PageDown_button(response_text):
-            logger.info(f'{worker_name}: page 1 found')
-            pages_to_scrape.append(Request('GET', f'https://secure.runescape.com/m={game_mode}/overall', params=params).prepare().url)
-            logger.debug(f'{worker_name}: worker exiting')
-            return 1 # TODO kill worker
+
+    # we start with a one-time check to speed the search up
+    # 1. if there are no ranked players
+    # 2. if page 1 has names but no page 2
+    # 3. page 80,000 exists (2m players)
+
     
-    # if page 80,000 exists return 80,000
-    params['page'] = 80000
-    response_text = await request_webpage(session, proxy, params, game_mode, worker_name)
-    if not is_page_one(response_text):
-        # save URLs to pages_to_scrape
-        # i've found that Request() takes 30s to prepare 80,000 URLs
-        # and since it's blocking i'm going to just Request() once and do the rest myself
-        logger.info(f'{worker_name}: page 80,000 found')
-        page_eightythousand_url = Request('GET', f'https://secure.runescape.com/m={game_mode}/overall', params=params).prepare().url
-        for page in range(1, 80000 + 1):
-            url = page_eightythousand_url.replace("page=80000", f"page={page}")
-            pages_to_scrape.append(url)
-        logger.info(f'{worker_name}: new pages_to_scrape length: {len(pages_to_scrape)}.  last entry: {pages_to_scrape[-1]}')
+    _params['page'] = 1
+    response_text = await request_webpage(session, proxy, _params, _game_mode, worker_name)
+    # if the game mode / category has no ranked players
+    if len(scrape_ranks(response_text)) == 0:
+        _params['page'] = 0
+        logger.info(f'{worker_name}: 0 pages in {game_mode} / {category}')
         logger.debug(f'{worker_name}: worker exiting')
-        return 0 # TODO kill worker
+        return 0
+    # if page 1 has players but there is no page 2
+    elif not contains_PageDown_button(response_text):
+        _params['page'] = 1
+        logger.info(f'{worker_name}: 1 pages in {game_mode} / {category}')
+        logger.debug(f'{worker_name}: worker exiting')
+        return 1
+    
+
+    _params['page'] = 80000
+    response_text = await request_webpage(session, proxy, _params, _game_mode, worker_name)
+    if game_mode == "Tournament" and category == 'Agility':
+        logger.debug(f'{worker_name}: Inside Tournament / Agility debugging')
+        logger.debug(f'{worker_name}: {response_text}') # TODO remove for debugging
+    # page 80,000 exists
+    if not is_page_one(response_text):
+        logger.info(f'{worker_name}: 80000 pages in {game_mode} / {category}')
+        logger.debug(f'{worker_name}: worker exiting')
+        return 80000
+
 
     # there is somewhere between 2 and 79,999 pages
     previous_pages = (1, 80000)
     while True:
-        # find the middle number between the ints in previous_pages, rounded down
+        # find the middle number between the values in previous_pages, rounded down
         page = floor(median(previous_pages))
         logger.debug(f'{worker_name}: trying page {page}')
 
-        params['page'] = page
-        response_text = await request_webpage(session, proxy, params, game_mode, worker_name)
+        _params['page'] = page
+        response_text = await request_webpage(session, proxy, _params, _game_mode, worker_name)
 
+        # the page we requested doesn't exist
         if is_page_one(response_text):
-            if contains_PageDown_button(response_text):
-                previous_pages = (previous_pages[0], page)
-                logger.debug(f'{worker_name}: less than {page}')
+            previous_pages = (previous_pages[0], page)
+            logger.debug(f'{worker_name}: less than {page} pages')
+        # requested page does exist
         else:
+            # there is more pages
             if contains_PageDown_button(response_text):
                 previous_pages = (page, previous_pages[1])
-                logger.debug(f'{worker_name}: more than {page}')
+                logger.debug(f'{worker_name}: more than {page} pages')
+            # this is the last page
             else:
-                logger.info(f'{worker_name}: found final page {page}')
-                # save URL to pages_to_scrape
-                # i've found that Request() takes 30s to prepare 80,000 URLs
-                # and since it's blocking i'm going to just Request() once and do the rest myself
-                page_eightythousand = params
-                page_eightythousand['page'] = 80000
-                page_eightythousand_url = Request('GET', f'https://secure.runescape.com/m={game_mode}/overall', params=page_eightythousand).prepare().url
-                for page in range(1, page + 1):
-                    url = page_eightythousand_url.replace("page=80000", f"page={page}")
-                    pages_to_scrape.append(url)
-                logger.info(f'{worker_name}: pages_to_scrape length: {len(pages_to_scrape)}.  last entry: {pages_to_scrape[-1]}')
+                logger.info(f'{worker_name}: {_params["page"]} pages in {game_mode} / {category}')
                 logger.debug(f'{worker_name}: worker exiting')
-                return 0 # TODO kill worker
+                return page
     
-#########
+
 
 async def main():
     # define a list of all the game_modes and params that we need to search for
@@ -179,22 +202,14 @@ async def main():
         for category in game_mode_categories[game_mode]: # overall
             # create a list of game_modes and categories
             list_of_gamemode_params.append({
-                'game_mode': game_mode_categories[game_mode][category]['game_mode'],
-                'params': game_mode_categories[game_mode][category]['params']
+                'game_mode': game_mode,
+                'params': category
         })
 
     async with aiohttp.ClientSession() as session:
         # get proxy list
-        proxies = ["http://erwvnich-dest:s8ix5905h935@209.127.191.180:9279",
-                   "http://erwvnich-dest:s8ix5905h935@45.95.96.132:8691",
-                   "http://erwvnich-dest:s8ix5905h935@45.95.96.187:8746",
-                   "http://erwvnich-dest:s8ix5905h935@45.95.96.237:8796",
-                   "http://erwvnich-dest:s8ix5905h935@45.136.228.154:6209",
-                   "http://erwvnich-dest:s8ix5905h935@45.94.47.66:8110",
-                   "http://erwvnich-dest:s8ix5905h935@45.94.47.108:8152",
-                   "http://erwvnich-dest:s8ix5905h935@193.8.56.119:9183",
-                   "http://erwvnich-dest:s8ix5905h935@45.95.99.226:7786",
-                   "http://erwvnich-dest:s8ix5905h935@45.95.99.20:7580", ]  # placeholder
+        proxies = await get_proxy_list(session)
+        proxies = proxies[0:2] # temporary debugging
 
         while len(list_of_gamemode_params) > 0:
             # create a mapping of proxies to inputs
@@ -217,7 +232,7 @@ async def main():
 
             # run workers
             logger.debug(f'starting a new batch of {len(working_list)} hiscores categories')
-            workers = [asyncio.create_task(create_worker(session=session, proxy=value['proxy'], worker_name=f'worker_{count}', game_mode=value['game_mode'], params=value['params'])) for count, value in enumerate(working_list)]            
+            workers = [asyncio.create_task(create_worker(session=session, proxy=value['proxy'], worker_name=f'worker_{count}', game_mode=value['game_mode'], category=value['params'])) for count, value in enumerate(working_list)]            
             await asyncio.gather(*workers)
 
 
@@ -225,11 +240,5 @@ async def main():
 pages_to_scrape = []
 
 asyncio.run(main())
-logger.error(pages_to_scrape)
-print(pages_to_scrape)
 
-# TODO never used pickle.  does this work?
-with open('outfile', 'wb') as file_handle:
-    pickle.dump(pages_to_scrape, file_handle)
-
-print('block')
+logger.info('Exiting')
